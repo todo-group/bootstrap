48b0bf6e757334b388680369975f0495e2e71b29
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2025-03-14 10:39:18 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,19 +2,20 @@
 
 DOT_SSH=${HOME}/.ssh
 
-if [ -f "${DOT_SSH}/authorized_keys" ]; then
-    echo "Error: ${DOT_SSH}/authorized_keys exists"
-    exit 127
-fi
-
 read -p "GitHub ID: " GITHUB_ID
 
-mkdir -p ${DOT_SSH}
-chmod 755 ${DOT_SSH}
-curl "https://github.com/${GITHUB_ID}.keys" > "${DOT_SSH}/authorized_keys"
-chmod 644 ${DOT_SSH}/authorized_keys
+if [ -f "${DOT_SSH}/authorized_keys" ]; then
+    echo "Info: ${DOT_SSH}/authorized_keys exists. Skip."
+else
+    mkdir -p ${DOT_SSH}
+    chmod 755 ${DOT_SSH}
+    curl "https://github.com/${GITHUB_ID}.keys" > "${DOT_SSH}/authorized_keys"
+    chmod 644 ${DOT_SSH}/authorized_keys
+fi
 
-if [ -f "${DOT_SSH}/config" ]; then :; else
+if [ -f "${DOT_SSH}/config" ]; then
+    echo "Info: ${DOT_SSH}/config exists. Skip."
+else
     cat > "${DOT_SSH}/config" << EOF
 Host *github.com
   User ${GITHUB_ID}
