103ccb1a64cd91f0b60610c7c491c5651567932c
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2025-03-28 14:22:23 +0900

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,19 +10,26 @@ ID_EXISTS=$(id "$USERNAME" > /dev/null 2>&1; echo $?)
 test "$ID_EXISTS" = 1 || { echo "Error: user $USERNAME exists. Exit."; exit 127; }
 
 # create user
+set -x
 useradd -d "$NEW_HOME" -m -s /bin/bash -U "$USERNAME"
+{ set +x; } 2>/dev/null
 passwd "$USERNAME"
+set -x
 usermod -aG sudo "$USERNAME"
+{ set +x; } 2>/dev/null
 
 read -r -p "GitHub ID: " GITHUB_ID
 test -n "$GITHUB_ID" || { echo "Info: skipping SSH key installation"; exit 0; }
 
 # setup ssh keys
 DOT_SSH="$NEW_HOME"/.ssh
+set -x
 mkdir -p "$DOT_SSH"
 chmod 755 "$DOT_SSH"
 curl https://github.com/"$GITHUB_ID".keys > "$DOT_SSH"/authorized_keys
 chmod 644 "$DOT_SSH"/authorized_keys
+{ set +x; } 2>/dev/null
+echo "Info: generating ssh config"
 cat > "$DOT_SSH"/config << EOF
 Host *github.com
     User ${GITHUB_ID}
@@ -30,5 +37,7 @@ Host *github.com
     ForwardX11Trusted no
     StrictHostKeyChecking no
 EOF
+set -x
 chmod 644 "$DOT_SSH"/config
 chown -R "$USERNAME":"$USERNAME" "$DOT_SSH"
+{ set +x; } 2>/dev/null
